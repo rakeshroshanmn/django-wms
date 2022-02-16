2dc99b96769a5cef5707dfb1ccfec8d7b6f081f0
rakeshroshanmn <rakeshroshan4411@gmail.com>
2022-02-16 19:26:37 +0530

    full screen fixed

--- warehouse/user/models.py
+++ warehouse/user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class user_data(models.Model):
+    username = models.CharField(max_length=10)
+    password = models.CharField(max_length=10)
